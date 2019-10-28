20e111b0667ee77a75c57cf6609e7a518a8f6803
V-V <vvp@haja.io>
2019-10-28 10:59:30 +0200

    Separate test cases for each fixture file

--- test/js2amb.spec.js
+++ test/js2amb.spec.js
@@ -4,7 +4,7 @@ const js2amb = require('../src')
 const fs = require('fs')
 const FIXTURES_PATH = 'test/fixtures/js/'
 const normalizeAmbientsCode = code => code.replace(/\r?\n\s*|\r\s*/g, '').replace(/\s+/g, ' ')
-const testFixtures = (path) => {
+const testFixtures = (path, tests) => {
   const fixtures = fs.readdirSync(path)
   const fixturePairs = fixtures
     .filter(item => item.endsWith('.ambient'))
@@ -19,20 +19,19 @@ const testFixtures = (path) => {
     console.log(`Compiling: ${path + filePair.js}`)
     const javascript = fs.readFileSync(path + filePair.js).toString().trim()
     const expectedAmbient = normalizeAmbientsCode(fs.readFileSync(path + filePair.ambient).toString())
-    assert.strictEqual(
-      js2amb(javascript),
-      expectedAmbient,
-      `Compiling ${filePair.js} didn't result in ${filePair.ambient}`
-    )
+    it(`${tests}: ${filePair.js}`, () => {
+
+      assert.strictEqual(
+        js2amb(javascript),
+        expectedAmbient,
+        `Compiling ${filePair.js} didn't result in ${filePair.ambient}`
+      )
+    })
 
   })
 }
 
 describe('JS Compiler', function () {
-  it('language concepts to ambient syntax', () => {
-    testFixtures(FIXTURES_PATH + 'lang_features/')
-  })
-  it('computation abstractions to ambient syntax', () => {
-    testFixtures(FIXTURES_PATH + 'abstractions/')
-  })
+  testFixtures(FIXTURES_PATH + 'lang_features/','language concepts')
+  testFixtures(FIXTURES_PATH + 'abstractions/', 'computation abstractions')
 })
