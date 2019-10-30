4cc95c433a241c09f5ae2228f7b3618a0b5748fd
V-V <vvp@haja.io>
2019-10-30 10:44:54 +0200

    Refactored switch-case conversion to a mapper-object

--- src/index.js
+++ src/index.js
@@ -23,23 +23,9 @@ const functionDefinition = (name, body) => ({
   }
 })
 
-const convertDeclaration = (astNode) => {
-  switch (astNode.type) {
-    case 'VariableDeclaration':
-      return astNode.declarations.map(convertDeclaration)
-    case 'VariableDeclarator':
-      switch (astNode.init.type) {
-        case 'ArrowFunctionExpression':
-          return functionDefinition(astNode.id.name,functionBody(astNode.init, literal(astNode.init.body.value)))
-      }
-  }
-}
-
 const programFile = (declarations, resultStatement) => ({
   toAlgebra: () => {
     const algebra = declarations
-      .reduce((acc,x) => acc.concat(x), [])
-      .filter(x => x !== undefined)
       .map(declaration => declaration.toAlgebra())
       .map(code => code.replace(/\r?\n\s*|\r\s*/g, '').replace(/\s+/g, ' '))
       .join("|")
@@ -47,17 +33,43 @@ const programFile = (declarations, resultStatement) => ({
   }
 })
 
-module.exports = function (js) {
-
-  let counter = 0
-  let log = (js) => (node, meta) => {
-    console.log(`${++counter}: ${node.type} (${js.substring(meta.start.offset, meta.end.offset)}) - (${Object.keys(node)})` )
-    if (node.type === 'ArrowFunctionExpression') {
-      console.log(`    - body: ${node.body.type}`)
+const astMapper = () => ({
+  mappers: {},
+  register: function (nodetype, func) {
+    this.mappers[nodetype] = func
+  },
+  lookupMap: new WeakMap(),
+  lookup: function (node) {
+    if (Array.isArray(node)) {
+      return node
+        .map((n) => this.lookupMap.get(n))
+        .reduce((acc,x) => acc.concat(x), [])
     }
+    return this.lookupMap.get(node)
+  },
+  parseAndMap: function (js) {
+    let counter = 0
+    let latestNode
+    let esprimaMapper = (node, meta) => {
+      console.log(`${++counter}: ${node.type} (${js.substring(meta.start.offset, meta.end.offset)}) - (${Object.keys(node)})` )
+      const mapFunc = this.mappers[node.type]
+      if (mapFunc === undefined)
+        return
+      latestNode = mapFunc(node)
+      this.lookupMap.set(node, latestNode)
+    }
+    esprima.parseScript(js, {}, esprimaMapper)
+    return latestNode
   }
+})
 
-  const node = esprima.parseScript(js, {}, log(js))
-  return programFile(node.body.map(convertDeclaration), null).toAlgebra()
-
+module.exports = function (js) {
+  let mapper = astMapper()
+  mapper.register('Literal', (node) => literal(node.value))
+  mapper.register('ArrowFunctionExpression', (node) => functionBody([], mapper.lookup(node.body)))
+  mapper.register('VariableDeclarator', (node) => functionDefinition(node.id.name, mapper.lookup(node.init)))
+  mapper.register('VariableDeclaration', (node) => mapper.lookup(node.declarations))
+  mapper.register('Program', (node) => programFile(mapper.lookup(node.body)))
+  return mapper.parseAndMap(js).toAlgebra()
+  
 }
